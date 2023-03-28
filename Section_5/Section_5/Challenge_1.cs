35549e1e77d7716e810fb8509f7ab76c4eb06663
amirOrbe <amir.orbe@bunsan.io>
2023-03-28 12:54:38 -0600

    Section_5

---
+++ Section_5/Section_5/Challenge_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static void ForLoop()
+        {
+            for (int i = -3; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void WhileLoop()
+        {
+            int initialValue = 3;
+            while (initialValue >= -3)
+            {
+                Console.WriteLine(initialValue);
+                initialValue--;
+            }
+        }
+
+        public static void Run()
+        {
+            WhileLoop();
+            ForLoop();
+
+        }
+
+    }
+}
---
+++ Section_5/Section_5/Challenge_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Challenge_2
+{
+    public class Challenge_2
+    {
+        public static void Main()
+        {
+            int i = -10;
+
+            while (true)
+            {
+
+                if (i % 3 == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                if (i == 10)
+                {
+
+                    break;
+                }
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
