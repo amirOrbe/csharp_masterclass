df1da6ac20e12e4f119860c141c92809c60a6b11
amirOrbe <amir.orbe@bunsan.io>
2023-04-01 19:48:55 -0600

    Section_12

---
+++ Section_12/Section_12/ChallengeDelegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Section_12
+{
+    public class Challenge_Delegate
+    {
+        public delegate float DelegateOper(float a, float b);
+
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+
+
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+        public static float ApplyOperation(float a, float b, DelegateOper oper)
+        {
+            float result;
+
+            result = oper(a, b);
+
+            return result;
+        }
+    }
+}
---
+++ Section_12/Section_12/LambdaChallenge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Run
+    {
+        static Func<float, float, float> Plus = (a, b) => a + b;
+        static Func<float, float, float> Minus = (a, b) => a - b;
+        static Func<float, float, float> Divide = (a, b) => a / b;
+        static Func<float, float, float> Multiply = (a, b) => a * b;
+
+        static public Dictionary<string, Func<float, float, float>> Operators = new Dictionary<string, Func<float, float, float>>();
+
+        static Run()
+        {
+            Operators.Add("-", Minus);
+            Operators.Add("+", Plus);
+            Operators.Add("/", Divide);
+            Operators.Add("*", Multiply);
+        }
+
+        public static Func<float, float, float> OperationGet(string s)
+        {
+            return Operators.ContainsKey(s) ? Operators[s] : null;
+        }
+    }
+}
