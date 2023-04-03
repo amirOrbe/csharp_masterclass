9a760d9b871bc3e0a8fce3dc6803663f77938e8d
amirOrbe <amir.orbe@bunsan.io>
2023-04-02 22:08:20 -0600

    Section_16

---
+++ Section_16/Section_16/Cats_challenge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Section_16
+{
+    public class Cats_challenge
+    {
+        public static int NotHungryCats(string kitchen)
+        {
+            var cats = kitchen.Replace(" ", "").Split('F');
+
+            var leftCats = cats[0].Where((x, i) => i % 2 == 1).Count(x => x == '~');
+            var rightCats = cats[1].Where((x, i) => i % 2 == 0).Count(x => x == '~');
+
+            return leftCats + rightCats;
+        }
+    }
+}
---
+++ Section_16/Section_16/SumOfTwoChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section_16
+{
+    public class SumOfTwoChallenge
+    {
+        public static int SumOfTwo(int[] nums, int SumToFind)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int result = 0;
+
+            foreach (int value in nums)
+            {
+                if (dic.ContainsKey(SumToFind - value) && dic[SumToFind - value] > 0)
+                {
+
+                    dic[SumToFind - value] -= 1;
+
+                    result++;
+                    continue;
+                }
+                if (dic.ContainsKey(value))
+                {
+                    dic[value] += 1;
+                }
+                else
+                {
+                    dic.Add(value, 1);
+                }
+            }
+            return result;
+        }
+    }
+}
